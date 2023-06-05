f1724f19993ee791430027f544506e485b820f6f
TinkyyWinki <107576816+TinkyyWinki@users.noreply.github.com>
2023-06-04 23:21:28 -0300

    02

---
+++ PooAula02/src/classes01/Classes01.java
@@ -0,0 +1,26 @@
+
+package classes01;
+
+public class Classes01 {
+
+
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        Caneta c2 = new Caneta();
+        c1.ponta = 0.7f;
+        c1.cor = "azul";
+        c1.tampada = false;
+        c1.modelo = "bic";
+        c1.carga = 80;
+        c1.tampar();
+        c1.status();
+        c1.rabiscar();
+        
+        c2.cor = "vermelha";
+        c2.ponta = 1.0f;
+        c2.carga = 90;  
+        c2.destampar();
+        c2.status();
+        c2.rabiscar();
+    }
+}
